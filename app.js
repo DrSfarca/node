c0803fa92818888f1ed72c12f0f46aa5ae583bca
Driton Sfarca <sfarca.driton@gmail.com>
2022-01-19 22:11:19 +0100

    some practice

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const amount = 12;
+// Every file is a module
+// Modules - Encapsulated Code  (only share minimum)
+const names = require("./4-names");
+const sayHi = require("./5-utils");
+const data = require("./6-alternative-flavour");
+require("./7-mind-grenade");
 
-if (amount < 10) {
-  console.log("small number");
-} else {
-  console.log(console.log("large number"));
-}
-
-console.log(`hey it's my first node app!!!`);
+sayHi("susan");
+sayHi(names.john);
+sayHi(names.peter);
